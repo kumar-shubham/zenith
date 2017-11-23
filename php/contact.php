4bfee87a03750f11180360893c650a392407f0e9
kumar-shubham <kb4shubham@gmail.com>
2017-11-23 23:22:56 +0530

    email changed

--- php/contact.php
+++ php/contact.php
@@ -39,7 +39,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@example.net";
-$address = "info@zenith.com";
+$address = "info@zenithindia.in";
 
 
 // Configuration option.
