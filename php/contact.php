17effd2eeb0a2fd1663a619d3e55da842b27b014
kumar-shubham <kb4shubham@gmail.com>
2017-11-24 00:13:52 +0530

    changes

--- php/contact.php
+++ php/contact.php
@@ -41,7 +41,7 @@ if(get_magic_quotes_gpc()) {
 //$address = "example@example.net";
 // $address = "info@zenithindia.in";
 
-$address = "kb4shubham@gmail.com";
+$address = "pesswanilisha@gmail.com";
 
 // Configuration option.
 // i.e. The standard subject will appear as, "You've been contacted by John Doe."
